b4bb0d9701c4dd154bd79ab92ea1ec10b6845ca0
Jordi <appsjcm@users.noreply.github.com>
2026-07-15 20:11:42 +0200

    Share VoiceICC PyInstaller build script

--- voiceicc/crear_exe.bat
+++ voiceicc/crear_exe.bat
@@ -2,6 +2,6 @@
 python -m pip install --upgrade pip
 python -m pip install -r requirements.txt
 python -m pip install pyinstaller
-python -m PyInstaller --noconsole --onefile --name VoiceICC --icon assets\app_icon.ico --add-data "assets;assets" --collect-all onnxruntime src\voiceicc.py
+python compilar_voiceicc.py
 echo EXE creado en dist\VoiceICC.exe
 pause
